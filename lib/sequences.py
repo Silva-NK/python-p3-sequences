50f65d099818b5fd65134daa269e21cb8b5538d3
Silva-NK <kiagonsuzanne@gmail.com>
2025-05-12 22:49:58 +0300

    Completes fibonacci sequences in the formats asked.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+
+    for i in range(length):
+        if i == 0:
+            fibonacci.append(0)
+        elif i == 1:
+            fibonacci.append(1)
+        else:
+            next_num = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(next_num)
+
+    print(fibonacci)
+print_fibonacci(12)
